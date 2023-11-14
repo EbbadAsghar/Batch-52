49916157678aa4971ff79634de6a32b925e6a80e
EbbadAsghar <ebbadasghar@gmail.com>
2023-11-14 11:15:45 +0500

    Q6

---
+++ Assignment 1/Q6.ts
@@ -0,0 +1,10 @@
+/* Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, "\t" and "\n", at least once. 
+Print the name once, so the whitespace around the name is displayed. 
+Then print the name after striping the white spaces. */
+
+// Whitespace around the name is displayed
+const person_name = "\tEbbad Asghar\n";
+console.log(person_name);
+// name after striping the white spaces
+console.log(person_name.trim());
