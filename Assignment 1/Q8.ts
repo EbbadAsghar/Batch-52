c900a74d9a50359685141a1be385cda8b7c8eeb4
EbbadAsghar <ebbadasghar@gmail.com>
2023-12-05 11:49:22 +0500

    updated

---
+++ Assignment 1/Q8.ts
@@ -0,0 +1,11 @@
+/*8. You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+for (let i = 0; i < console.log.length; i++) {
+  console.log(i);
+}
