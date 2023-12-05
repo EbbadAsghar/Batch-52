5aa4b9aa4d278e9eb8e7aff45ca99eb4fbdcee95
EbbadAsghar <ebbadasghar@gmail.com>
2023-12-05 12:02:55 +0500

    updated

---
+++ Assignment 1/Q10.ts
@@ -0,0 +1,22 @@
+/* 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+
+// Written By: Ebbad    Dated:05-12-2023
+/*This program repeatedly prints the sum of 5 and 3, then iterates over the length of the console.log function
+and prints each index.*/
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+for (let i = 0; i < console.log.length; i++) {
+  console.log(i);
+}
+
+// Written By: Ebbad    Dated:05-12-2023
+/*This program simply store the statement in variable named 'firstName' and then print the output.*/
+let firstName = `Hello Eric, would you like to learn some Python today?`;
+console.log(firstName);
+
+export {};
