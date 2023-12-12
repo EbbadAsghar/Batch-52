a301bd8c843e5aee3efe50de81e92492de066119
Ebbad Dhami <ebbadasghar@gmail.com>
2023-12-12 14:49:00 +0500

    updated

--- Assignment 1/Q16.ts
+++ Assignment 1/Q16.ts
@@ -24,13 +24,13 @@ for (let i = 0; i < guest_name2.length; i++) {
 //• Use append() to add one new guest to the end of your list.
 //• Print a new set of invitation messages, one for each person in your list.
 
-let guest_name2 = ['Ali', 'Ahmad', 'Rehman'];
+/*let guest_name2 = ['Ali', 'Ahmad', 'Rehman'];
 let more_guest = guest_name2.push('Adnan', 'Bilal', 'Razzaq')
 let new_guest = guest_name2.unshift('Haider')
 let new_guest1 = guest_name2.splice(3,0,'Israr')
 let new_guest2 = guest_name2.push('Babar')
 for (let i = 0; i < guest_name2.length; i++) {
     console.log(`Hi ${guest_name2[i]}, You are invited on Dinner.`)
-    }
+    } */
 
 export{}
