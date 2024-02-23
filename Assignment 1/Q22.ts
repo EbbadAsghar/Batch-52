32a17783669d094777c9ec57532ce51da20dfcdb
EbbadAsghar <ebbadasghar@gmail.com>
2024-02-23 08:48:10 +0500

    Updated

---
+++ Assignment 1/Q22.ts
@@ -0,0 +1,9 @@
+/* Q22) Intentional Error:  
+If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+Change an index in one of your programs to produce an index error. Make sure you correct the error 
+before closing the program.*/
+
+
+let cities: string [] = ["Lahore", "Islamabad", "Karachi", "Sargodha"];
+console.log(cities[4]); //intentional error
+console.log(cities[2]);
