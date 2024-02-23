f2f3d3b033ecda2292d7ced313e2b29e10e5f1a8
EbbadAsghar <ebbadasghar@gmail.com>
2024-02-23 08:43:56 +0500

    Updated

---
+++ Assignment 1/Q21.ts
@@ -0,0 +1,5 @@
+/* Q21) They think of something you could store in a TypeScript Object. Write a program that creates 
+Objects containing these items.*/
+
+let data : {firstname:string,lastname:string,age:number,gender:string} = {firstname : "Ahmad",lastname : "Ali",age : 30,gender : "Male",};
+console.log(data);
---
+++ Assignment 1/dist/Q21.js
@@ -0,0 +1,6 @@
+"use strict";
+/* Q21) They think of something you could store in a TypeScript Object. Write a program that creates
+Objects containing these items.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let data = { firstname: "Ahmad", lastname: "Ali", age: 30, gender: "Male", };
+console.log(data);
