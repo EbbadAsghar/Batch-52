b6ab0bc458606b5a683a2c7c5ba4b17d6ad62780
EbbadAsghar <ebbadasghar@gmail.com>
2024-02-28 10:51:50 +0500

    updated

--- Assignment 1/dist/Q25.js
+++ Assignment 1/dist/Q25.js
@@ -9,14 +9,11 @@ fails. (The version that fails will have no output.) */
 Object.defineProperty(exports, "__esModule", { value: true });
 let alien_color = 'green';
 // Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
-// if (alien_color === "green") {
-//     console.log(`You have earned 5 points.`)
-// }
+if (alien_color === "green") {
+    console.log(`You have earned 5 points.`);
+}
 // • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output).
 alien_color = 'red';
 if (alien_color === "green") {
     console.log(`You have earned 5 points.`);
-}
-else {
-    console.log("Yes!!");
 }
