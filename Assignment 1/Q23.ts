a4ab3be27838742562ec8586bcef9b7dc5528082
EbbadAsghar <ebbadasghar@gmail.com>
2024-02-28 09:24:09 +0500

    updated

---
+++ Assignment 1/Q23.ts
@@ -0,0 +1,36 @@
+/* Q23) Conditional Tests:  
+Write a series of conditional tests. Print a statement describing each test and your prediction 
+for the results of each test. Your code should look something like this: 
+let car = 'subaru'; 
+console.log("Is car == 'subaru'? I predict True.") 
+console.log(car == 'subaru') 
+• Look closely at your results, and make sure you understand why each line evaluates to True or False. 
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+
+
+let car:string = 'subaru'; 
+console.log("Is car == 'subaru'? I predict True oterwise it's false."); 
+console.log(car == 'subaru');
+console.log(car == 'suzuki');
+
+let city:string = 'Lahore'; 
+console.log("Is city == 'Lahore'? I predict True oterwise it's false."); 
+console.log(city == 'Lahore');
+console.log(city == 'Karachi');
+
+let fruit:string = 'Pineapple'; 
+console.log("Is fruit == 'pineapple'? I predict True oterwise it's false."); 
+console.log(fruit == 'Pineapple');
+console.log(fruit == 'mango');
+
+let country:string = 'Pakistan'; 
+console.log("Is country == 'Pakistan'? I predict True oterwise it's false."); 
+console.log(country == 'Pakistan');
+console.log(country == 'Russia');
+
+let num:any = 13; 
+console.log("Is num == 13 ? I predict True oterwise it's false."); 
+console.log(num === 13);
+console.log(num === "23");
+
+
---
+++ Assignment 1/dist/Q23.js
@@ -0,0 +1,35 @@
+"use strict";
+/* Q23) Conditional Tests:
+Write a series of conditional tests. Print a statement describing each test and your prediction
+for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+Object.defineProperty(exports, "__esModule", { value: true });
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True oterwise it's false.");
+console.log(car == 'subaru');
+console.log(car == 'suzuki');
+let city = 'Lahore';
+console.log("Is city == 'Lahore'? I predict True oterwise it's false.");
+console.log(city == 'Lahore');
+console.log(city == 'Karachi');
+let fruit = 'Pineapple';
+console.log("Is fruit == 'pineapple'? I predict True oterwise it's false.");
+console.log(fruit == 'Pineapple');
+console.log(fruit == 'mango');
+let country = 'Pakistan';
+console.log("Is country == 'Pakistan'? I predict True oterwise it's false.");
+console.log(country == 'Pakistan');
+console.log(country == 'Russia');
+let num = 13;
+console.log("Is num == 13 ? I predict True oterwise it's false.");
+console.log(num === 13);
+console.log(num === "23");
+// False Tests
+let num1 = 13;
+console.log("Is num == 13 ? I predict True oterwise it's false.");
+console.log(num === 3);
+console.log(num === "23");
